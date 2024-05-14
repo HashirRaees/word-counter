563396264eb580b55e319f8b1c59882bc84a3aa7
HashirRaees <163395402+HashirRaees@users.noreply.github.com>
2024-05-14 22:52:35 +0500

    a simple word counter just enter a text and it will count the words in it

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.magentaBright("\n\t Word Counter By Hashir Raees"));
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words"
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
